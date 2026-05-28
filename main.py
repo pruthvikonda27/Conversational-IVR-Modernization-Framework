205ead4b8f1687053d9d6d17152764b19ec34ccd
pruthvikonda27 <pruthvikonda575@gmail.com>
2026-05-28 09:47:01 +0530

    fixed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Milestone2.backend import app
+from fastapi import FastAPI
 
-# app=FastAPI()
+app = FastAPI()
 
-# @app.get("/")
-# def home():
-#     return {"message": "API Working"}
+@app.get("/")
+def home():
+    return {"status": "working"}
